48339620209619f0117c00cf7cd593389de361f7
M. Charan <114210807+2100030351kl@users.noreply.github.com>
2024-04-27 05:45:23 +0530

    I have commited my project

---
+++ backend/data.js
@@ -0,0 +1,93 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'Pooji',
+      email: 'pooji@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: '1',
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Women',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+      size:'small',
+      
+    },
+    {
+      // _id: '2',
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Women',
+      image: '/images/p2.jpg',
+      price: 555,
+      countInStock: 0,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+      size:'small',
+    },
+    {
+      // _id: '3',
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Men',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+      size:'small',
+    },
+    {
+      // _id: '4',
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Men',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'A high quality product',
+      size:'small',
+    },
+    {
+      // _id: '5',
+      name: 'Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Men',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+      size:'small',
+    },
+    
+  ],
+};
+export default data;
